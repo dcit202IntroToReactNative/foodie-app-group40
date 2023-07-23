83c74db6fe3746a0718442468cc1ab619a5d72ee
Isaac Adumoah <sduisaac@yahoo.com>
2023-07-23 14:00:32 +0000

    final commit

--- constants/index.js
+++ constants/index.js
@@ -1,41 +1,41 @@
 export const categories = [
-  "Burger",
+  "Kenkey",
   "Pizza",
-  "Sushi",
-  "Kebab",
-  "Sweets",
+  "Waakye",
+  "Fries",
+  "Fried Yam",
   "Noodles",
-  "Karahi"
+  "Beans"
 ]
 
 export const foodItems = [
   {
-    name: 'Cheezy Pizza',
-    price: '13.40',
-    ingredients: 'crust, pepperoni, sausage',
-    desc: 'A mouthwatering pizza is a culinary wonder that delights your senses and satisfies your hunger. The crispy crust is the foundation of this masterpiece, topped with a tangy tomato sauce that perfectly balances the flavors.',
-    image: require('../assets/images/pizza.png'), 
+    name: 'Kenkey',
+    price: '10.00',
+    ingredients: 'oil, corn, grain',
+    desc: 'A mouthwatering kenkey is a culinary wonder that delights your senses and satisfies your hunger. The taste of kenkey is unique and distinctive, with a flavor profile that sets it apart from other traditional dishes, topped with grounded pepper and tomato and any fish of your choice.',
+    image: require('../assets/images/kenkey.png'), 
   },
   {
-    name: 'Hamburger',
-    price: '18.30',
-    ingredients: 'Kinoa, kani, avocado',
-    desc: 'A delicious burger is a culinary masterpiece that satisfies the taste buds and leaves you craving for more. A succulent beef patty, grilled to perfection, is nestled between two soft, toasted buns. The patty is generously topped with melted cheese, crispy bacon, lettuce, tomato, and a special sauce that gives it a unique flavor. ',
-    image: require('../assets/images/burger.png'),
-  },
-  {
-    name: 'Sushi',
+    name: 'Fried Rice',
     price: '20.00',
-    ingredients: 'Rice, sausage, fish',
-    desc: "The rice is rolled into bite-sized pieces, with a thin sheet of nori seaweed wrapped around it. Inside, you'll find a range of delicacies such as salmon, tuna, crab, avocado, cucumber, or shrimp. The sushi is served with soy sauce, wasabi, and pickled ginger to enhance the flavors. Each piece of sushi is a work of art.",
-    image: require('../assets/images/sushi.png'),
+    ingredients: 'cooked rice,chicken, carrot, soy sauce,sesame oil',
+    desc: 'A delicious fried rice is a culinary masterpiece that satisfies the taste buds and leaves you craving for more. A succulent chicken, grilled to perfection, cooked with some of your favorite vegetables like tomatoes, lettuce, carrots and a spice of ginger to give you the mouthwatering taste you deserve. ',
+    image: require('../assets/images/friedrice.png'),
   },
   {
-    name: 'Ramen Noodle',
-    price: '10.50',
-    ingredients: 'Eggs, chicken, noodle',
-    desc: 'Ramen is a popular Japanese dish that consists of chewy noodles in a savory broth, topped with a range of flavorful ingredients. The broth can be made with pork, chicken, or vegetable stock, flavored with soy sauce, miso, or other seasonings. The noodles are cooked until they are tender and then added to the broth along with toppings such as slices of pork belly.',
-    image: require('../assets/images/ramen.png'),
+    name: 'Fries',
+    price: '15.00',
+    ingredients: 'Fried yam, ketchup, chicken ',
+    desc: "This deliciously fried yam with ketchup and chicken will leave you mouth watering and begging for more, it has been carefully by our cooks just to satisfy your hunger and make you crave more. The ingredients here are quite simple but very delicious in your mouth. ",
+    image: require('../assets/images/fries.png'),
+  },
+  {
+    name: 'Pizza',
+    price: '30.00',
+    ingredients: 'Pepperoni, sausage, pepper, mushroom, Tomatoes, Ham',
+    desc: 'Pizza is the epitome of mouthwatering indulgence! The sight of its golden, crispy crust topped with a luscious layer of vibrant tomato sauce and oozy melted cheese is enough to make anyones taste buds dance with anticipation. As you take that first bite, the explosion of flavors is nothing short of a culinary revelation.',
+    image: require('../assets/images/pizza.png'),
   },
 
 ]
